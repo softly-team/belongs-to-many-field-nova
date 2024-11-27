40196f283a11760a09cac73d6496ad2ae8e6d968
scramatte <scramatte@id.nixus.es>
2024-11-27 17:38:58 +0100

    commit various fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,7 @@ require('./nova.mix')
 mix
     .setPublicPath("dist")
     .js("resources/js/field.js", "js")
-    .vue({version: 3})
+    .vue({version: 3, extractStyles: 'css/vue.css' })
+    .css("resources/css/field.css", "css")
     .sass("resources/sass/field.scss", "css")
     .nova('benjacho/belongs-to-many-field-nova');
